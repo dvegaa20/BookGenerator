43df819e67861320b5efcd259a5864266c9fd1ed
dvegaa20 <diego.vega.camacho@gmail.com>
2025-01-29 10:35:09 -0600

    feat(types): extend type definitions with Region, Review, LanguageProps, and SeedProps interfaces

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,3 +1,13 @@
+declare interface Region {
+  label: string;
+  value: string;
+}
+
+declare interface Review {
+  author: string;
+  text: string;
+}
+
 declare interface GenerateBooksParams {
   seed: number;
   region: string;
@@ -10,8 +20,29 @@ declare interface Book {
   index: number;
   isbn: string;
   title: string;
-  authors: string[];
+  authors: string;
+  publishedYear: number;
   publisher: string;
   likes: number;
+  rating: number;
+  reviews: Review[];
+}
+
+declare interface LanguageProps {
+  selectedRegion: string;
+  onChange: (region: string) => void;
+  regions: Region[];
+}
+
+declare interface SeedProps {
+  seed: number;
+  onChange: (seed: number) => void;
+  onGenerate: () => void;
+}
+
+declare type SliderProps = React.ComponentProps<typeof Slider>;
+
+declare interface NumberReviewsProps {
   reviews: number;
+  onChange: (value: number) => void;
 }
