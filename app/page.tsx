a4d44b220456dd50be5e698e614855c8d5ec9013
dvegaa20 <diego.vega.camacho@gmail.com>
2025-01-29 10:35:49 -0600

    feat(page): enhance homepage with welcome message and CollapsibleTable component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
-import Image from "next/image";
+import { CollapsibleTable } from "@/components/table/Table";
+import React from "react";
 
 export default function Home() {
   return (
-    <div>
-      <p className="text-2xl text-white">Hello</p>
+    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
+      <p className="text-4xl font-semibold text-gray-800 my-16">
+        Welcome to the Book Generator App!
+      </p>
+
+      <div className="w-full max-w-[1200px] p-6 bg-white shadow-lg rounded-lg border border-gray-200 mb-8">
+        <CollapsibleTable />
+      </div>
     </div>
   );
 }
